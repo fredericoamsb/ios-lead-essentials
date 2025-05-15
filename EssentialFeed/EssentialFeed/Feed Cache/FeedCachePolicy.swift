3087fec42ae0fdfbd0d2947b4dfa781eb454f6de
Frederico Batista <sysfrederico@HAVAN.onmicrosoft.com>
2025-05-15 19:08:15 -0300

    Do persistence module part 9

---
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,25 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Fred on 15/05/25.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    private init() {}
+
+    private static let calendar = Calendar(identifier: .gregorian)
+
+    private static var maxCacheAgeInDays: Int {
+        return 7
+    }
+
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+}
